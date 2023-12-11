fc07af726f2d071afd640e09ab858551da069667
kevingg601 <kevingg601@gmail.com>
2023-12-11 10:42:19 +0000

    加入繪圖過程影片

--- replit.nix
+++ replit.nix
@@ -1,8 +1,14 @@
 { pkgs }: {
   deps = [
+    pkgs.libev
     pkgs.vimHugeX
     pkgs.gd
     pkgs.gnuplot
     pkgs.raylib
   ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.libev
+    ];
+  };
 }
